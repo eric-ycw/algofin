5f081ef72cd8e7df7abf214af16e86d320c1f512
Eric Yip <ericycw512@gmail.com>
2021-04-25 22:38:18 +0800

    Add actions argument to fetch_data

--- algofin/data.py
+++ algofin/data.py
@@ -1,9 +1,9 @@
 import yfinance as yf
 import pandas as pd
 
-def fetch_data(ticker, start_date, end_date):
-    return yf.Ticker(ticker).history(start=start_date, end=end_date)
+def fetch_data(ticker, start_date, end_date, actions=True):
+    return yf.Ticker(ticker).history(start=start_date, end=end_date, actions=actions)
 
 def risk_free_rate(start_date, end_date):
     # 13-week Treasury
-    return yf.Ticker('^IRX').history(start=start_date, end=end_date)
+    return yf.Ticker('^IRX').history(start=start_date, end=end_date, actions=False)
