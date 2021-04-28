ededa16b13c3587571e16cc364deb7cfdb8fdf74
Eric Yip <ericycw512@gmail.com>
2021-04-28 22:49:25 +0800

    Add ROC

--- algofin/indicators.py
+++ algofin/indicators.py
@@ -40,26 +40,10 @@ def plot_RSI(rsi, overbought=70, oversold=30):
     plt.legend()
     plt.show()
 
+def ROC(close, t=21):
+    return 100 * (close - close.shift(t)) / close.shift(t)
+
 def WilliamsR(price, high, low, t=14):
     t_high = high.rolling(window=t, min_periods=t).apply(max)
     t_low = low.rolling(window=t, min_periods=t).apply(min)
     return -100 * (t_high - price) / (t_high - t_low)
-
-def fill_indicators(df, normalization=None):
-    indicators = ['EMA_15', 'MACD', 'StochasticK', 'StochasticD', 'RSI', 'WilliamsR']
-
-    df['ClosePctChange'] = df['Close'].pct_change()
-    df['EMA_15'] = EMA(df['Close'], t=15)
-    df['MACD'] = MACD(df['Close'])
-    df['StochasticK'] = StochasticK(df['Close'], df['High'], df['Low'])
-    df['StochasticD'] = StochasticD(df['StochasticK'])
-    df['RSI'] = RSI(df, df['Close'])
-    df['WilliamsR'] = WilliamsR(df['Close'], df['High'], df['Low'])
-
-    for i in indicators:
-        if normalization == 'mean':
-            df[i] = mean_normalization(df[i])
-        elif normalization == 'minmax':
-            df[i] = min_max_normalization(df[i])
-
-    return df
